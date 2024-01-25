511ef5cc4fb8a2d099d0aac7b5594bbdc03ec925
Marvin Buss <marvin.buss@gmail.com>
2024-01-25 15:43:40 +0100

    lint

--- code/infra/search.tf
+++ code/infra/search.tf
@@ -15,7 +15,7 @@ resource "azurerm_search_service" "search_service" {
   hosting_mode                             = "default"
   local_authentication_enabled             = true
   partition_count                          = 1
-  public_network_access_enabled            = false # Can be disabled in production if users don't use the Azure Open AI studio 
+  public_network_access_enabled            = false # Can be disabled in production if users don't use the Azure Open AI studio
   replica_count                            = 1
   sku                                      = "basic"
 }
